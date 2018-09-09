2ec661244a493f9dae47a107513ccea3638ec5db
SeijiNishiwaki <nishiwakiat.wjnisi@gmail.com>
2018-09-09 20:06:41 +0900

    【課題】イベント装飾子の利用

--- base/event.js
+++ base/event.js
@@ -13,10 +13,17 @@ let event = new Vue({
          * 自身のプロパティを変更するメソッドを設定します。
          * 今回はクリックイベントに対してこのメソッドを呼び出します
          * 参照: event.html
+         * 
+         * イベントオブジェクトとは別に、任意のデータを受け取る場合は
+         * 任意の引数を設定します。テンプレート側から$eventと設定されたものがイベントオブジェクトとして渡ってきます
          */
-        insertDate: function(eventObject){
+        insertDate: function(anyData, eventObject){
+            console.log(anyData); // 引数を設定するとeventオブジェクトが渡されるので利用できます
             console.log(eventObject); // 引数を設定するとeventオブジェクトが渡されるので利用できます
             this.message = new Date().toLocaleString() // 自身のプロパティを書き換えると紐づくDOMも再描画されます
+        },
+        clickHandler: function(message) {
+            alert(`${message} でクリックイベントを捕捉しました`)
         }
     }
 });
